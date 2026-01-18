468c8dbcaeaeb2ace9c5fc09c662920e8e7f74e9
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-01-18 00:42:13 +0000

    Fix whitenoise middleware typo

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'  # added this for static files
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # added this for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
