b73cbd4d52504ec3907ad64c87744286f2e60ff5
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-01-18 01:31:18 +0000

    Remove legacy code

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from .models import Project, Skill, Contact
 from .serializers import ProjectSerializer, SkillSerializer, ContactSerializer
-from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 
 # Create your views here.
 # Class based views
@@ -10,8 +9,6 @@ from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 class ProjectListView(ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    parser_classes = [JSONParser, FormParser,
-                      MultiPartParser]  # allow empty GET requests
 
 
 class ProjectDetailView(RetrieveAPIView):
